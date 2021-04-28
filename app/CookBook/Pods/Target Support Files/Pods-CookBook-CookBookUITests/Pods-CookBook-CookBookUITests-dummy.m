c3e334de1745a7620fe0a6d98ec527ef2f4fb719
Jainam Shah <jainam.2000@yahoo.com>
2021-04-28 00:30:49 -0500

    removed Pod directory

--- app/CookBook/Pods/Target Support Files/Pods-CookBook-CookBookUITests/Pods-CookBook-CookBookUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CookBook_CookBookUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CookBook_CookBookUITests
-@end
--- app/CookBook/Pods/Target Support Files/Pods-CookBookTests/Pods-CookBookTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CookBookTests : NSObject
-@end
-@implementation PodsDummy_Pods_CookBookTests
-@end
