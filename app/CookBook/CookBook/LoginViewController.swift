aeb921ead86664d42fd0544d5df3255b1ad7a786
JPL-ST-Spring2020 <JPL-ST-000@JPL-ST-Spring2020.local>
2021-05-02 00:37:57 -0700

    Added getUser from coredata method

--- app/CookBook/CookBook/LoginViewController.swift
+++ app/CookBook/CookBook/LoginViewController.swift
@@ -12,12 +12,26 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var continueAsExistingUserButton: UIButton!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var continueAsNewUserButton: UIButton!
+    var users:[User]?
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // try to retrive user object from local core data
         // if user continueAsExistingUserButton
         // else enterNameTextField & continueAsNewUserButton
         continueAsExistingUserButton.isHidden = true;
+    }
+    
+    //Function to fetch the user from core data
+    
+    func getUser(){
+        do{
+            self.users = try context.fetch(User.fetchRequest())
+        }
+        catch{
+            
+        }
     }
 
 
