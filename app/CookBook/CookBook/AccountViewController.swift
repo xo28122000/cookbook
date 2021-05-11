272c786e9bba8cd0fdf24a659e0f7470d1b16ab0
Jainam Shah <jainam.2000@yahoo.com>
2021-05-11 01:38:09 -0500

    account vc init

---
+++ app/CookBook/CookBook/AccountViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AccountViewController.swift
+//  CookBook
+//
+//  Created by J on 5/11/21.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+    @IBOutlet weak var greetingLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // update greeting
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
